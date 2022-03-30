485836f9cad96621314b6835884a88fd71fcb020
tataganesh <tataganesh95@gmail.com>
2022-03-30 17:04:58 -0600

    FEAT: C Source file conversion

--- pytorch-to-tflite/convert.sh
+++ pytorch-to-tflite/convert.sh
@@ -6,3 +6,20 @@ python onnx_to_tflite.py
 #Testing
 echo "Testing"
 python run_tflite_model.py
+
+
+# Convert to CC
+echo "Generating C Source file"
+MODEL_TFLITE="model.tflite"
+MODEL_TFLITE_MICRO="model.cc"
+
+apt-get install xxd
+# Convert to a C source file, i.e, a TensorFlow Lite for Microcontrollers model
+xxd -i $MODEL_TFLITE > $MODEL_TFLITE_MICRO
+# Update variable names
+REPLACE_TEXT=${MODEL_TFLITE//./_}
+sed -i 's/'$REPLACE_TEXT'/g_model/g' $MODEL_TFLITE_MICRO
+
+echo "C Source file"
+# cat $MODEL_TFLITE_MICRO
+echo "$(cat $MODEL_TFLITE_MICRO)"
