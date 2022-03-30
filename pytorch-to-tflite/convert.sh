ccb7a7e2542c42b006843bb7c3416909267b021b
tataganesh <tataganesh95@gmail.com>
2022-03-30 17:22:50 -0600

    DOCS: update conversion script with prints

--- pytorch-to-tflite/convert.sh
+++ pytorch-to-tflite/convert.sh
@@ -3,6 +3,8 @@ echo "Converting"
 python torch_to_onnx.py --pytorch_model_path $1 
 python onnx_to_tflite.py
 
+echo "Conversion Completed. Saved as model.tflite"
+
 #Testing
 echo "Testing"
 python run_tflite_model.py
@@ -20,6 +22,8 @@ xxd -i $MODEL_TFLITE > $MODEL_TFLITE_MICRO
 REPLACE_TEXT=${MODEL_TFLITE//./_}
 sed -i 's/'$REPLACE_TEXT'/g_model/g' $MODEL_TFLITE_MICRO
 
-echo "C Source file"
+echo "C Source file generated. Saved as model.cc"
+
+# echo "C Source file"
 # cat $MODEL_TFLITE_MICRO
-echo "$(cat $MODEL_TFLITE_MICRO)"
+# echo "$(cat $MODEL_TFLITE_MICRO)"
